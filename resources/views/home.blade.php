d0752cb9d6828a5b8055feb8b2932bdd8d9e7cb1
Opone Chukwuyenum <oponechukwuyenum@icloud.com>
2020-05-06 04:59:51 +0100

    m

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,25 @@
 @extends('layouts.app')
 
+@section('title')
+    <title>{{ config('app.name', 'Laravel') }}</title>
+@endsection
+
 @section('content')
-<div class="d-flex justify-content-end mb-2">
-    <a href="{{route('discussions.create')}}" class="btn btn-success">Add Discussions</a>
-</div>
-
-<div class="card">
-    <div class="card-header">Dashboard</div>
-
-    <div class="card-body">
-        @if (session('status'))
-            <div class="alert alert-success" role="alert">
-                {{ session('status') }}
-            </div>
-        @endif
-
-        You are logged in!
+    <div class="d-flex justify-content-end mb-2">
+        <a href="{{route('discussions.create')}}" class="btn btn-success">Add Discussions</a>
     </div>
-</div>
+
+    <div class="card">
+        <div class="card-header">Dashboard</div>
+
+        <div class="card-body">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
+
+            You are logged in!
+        </div>
+    </div>
 @endsection
